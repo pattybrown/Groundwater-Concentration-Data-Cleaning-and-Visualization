40d3fe1a8513013dc35e4f0aab159b0ba09a4b20
Pat Brown <126992136+pattybrown@users.noreply.github.com>
2024-01-16 06:14:07 -0900

    Update Groundwater Concentration Data Cleaning and Visualization.R

--- Groundwater Concentration Data Cleaning and Visualization.R
+++ Groundwater Concentration Data Cleaning and Visualization.R
@@ -10,12 +10,14 @@ require(ggplot2)
 require(writexl)
 
 # Import datasets (hydrocarbon constituent concentrations, sulfate concentrations, sulfate injections):
-
 Data <- EQ_Shop_PHC_Data
 sulfate_injections_data <- sulfate_injections_dataset
 sulfate_data <- Sample_Data_Sulfate
 
-# Convert to appropriate data types: 
+
+      #### Cleaning the Data dataset (hydrocarbon constituent concentrations):
+
+# Format Data class: 
 Data$Date <- as.Date(Data$`Date Sampled`)
 Data$MW <- as.factor(Data$`Monitoring Well`)
 
@@ -26,13 +28,13 @@ Data$MW <- as.factor(Data$`Monitoring Well`)
 detcol <- function(x){
   ifelse(grepl("ND", x), 0, 1)}
 
-# Write function to remove NDs and round each result to 5 decimal places:
+# Write function to remove "ND"s from the dataframe and round each concentration result to 5 decimal places:
 cleancols <- function(c){
   gsub("ND", "", c) %>% as.numeric(c, na.rm = FALSE, 5) %>%
     round(5)}
 
 
-# Clean and format data for import into ProUCL:
+# Clean and format Data for compatability with ProUCL statistical software:
 
 # Create detections column for each constituent: 
 Data$GRO_detections <- detcol(Data$GRO)
@@ -50,7 +52,7 @@ Data$Toluene <- cleancols(Data$Toluene)
 Data$Ethylbenzene <- cleancols(Data$Ethylbenzene)
 Data$`Total Xylenes` <- cleancols(Data$`Total Xylenes`)
 
-# Filter to remove duplicate samples, keeping highest result:
+# Filter to remove duplicate samples, keeping only the highest result:
 Data <- Data %>% group_by(MW, Date) %>% filter(GRO == max(GRO)) %>% 
   filter(Benzene == max(Benzene)) %>% filter(Toluene == max(Toluene)) %>% filter(Ethylbenzene == max(Ethylbenzene)) %>%
   filter(`Total Xylenes` == max(`Total Xylenes`))
@@ -59,31 +61,14 @@ Data <- Data %>% group_by(MW, Date) %>% filter(GRO == max(GRO)) %>%
 Data$total_hydrocarbons <- as.numeric(Data$GRO + Data$DRO + Data$Benzene + Data$Toluene + 
                                         Data$Ethylbenzene + Data$`Total Xylenes`)
 
-# Organize columns and write cleaned dataframe to excel:
+# Organize columns and write cleaned Data dataframe to excel:
 Data2 <- Data[,c(1,16,3,18,5,19,7,20,9,21,11,22,13,23,24)]
 write_xlsx(Data2, "Cleaned Data")
 
-# Clean sulfate injection data:
-sulfate_injections_data <- clean_names(sulfate_injections_data)
-sulfate_injections_data$year <- ymd(sulfate_injections_data$year, truncated = 2L) #convert to date
-sulfate_injections_data$MW <- as.factor(sulfate_injections_data$well_id_injection_type)
 
-# Format sulfate injection dataframe for use in ggplot:
-injdates <- sulfate_injections_data %>% filter(type %in% c("Sulfate injection"))
-injdates$type <- as.factor(injdates$type)
-injdatesonly <- as.data.frame(injdates$year)
-injdatesonly <- as.data.frame(injdates$year) %>% mutate(inj_date = injdates$year)
-pushpulldates <- sulfate_injections_data %>% filter(type %in% c("Push-pull"))
-pushpulldates$type <- as.factor(pushpulldates$type)
-pushpulldatesonly <- as.data.frame(pushpulldates$year)
-pushpulldatesonly <- as.data.frame(pushpulldates$year) %>% mutate(push_date = pushpulldates$year)
+# Create time-series visualizations of Data by Monitoring Well:
 
-
-
-
-# Create time-series visualizations of data by Monitoring Well:
-
-# Faceted fixed-scale:
+# Faceted fixed-scale (PLOT 1):
 ggplot(Data, aes(Date, total_hydrocarbons, color = MW)) +
   geom_line(na.rm = TRUE) +
   xlim(as.Date(c('1/1/2008', '9/9/2023'), format="%d/%m/%Y")) +
@@ -96,7 +81,7 @@ ggplot(Data, aes(Date, total_hydrocarbons, color = MW)) +
   ylab("Total Hydrocarbons (mg/L)") 
 
 
-# Plot by injection well only:
+# Plot by injection well only (PLOT 2):
 inj <- Data %>% group_by(MW) %>% group_by(Date) %>% filter(MW %in% c("A","B","C","D","E","F","G","H","L"))
 
 ggplot(inj, aes(Date, total_hydrocarbons, color = MW)) +
@@ -111,31 +96,51 @@ ggplot(inj, aes(Date, total_hydrocarbons, color = MW)) +
   scale_x_date(date_breaks = "4 years", date_labels = "%Y", 
                limits = as.Date(c("2008-01-01", "2024-01-01"))) 
 
-##############
-# Sulfate Data: 
 
+#####################################
+
+      ### Cleaning, summarizing and visualizing sulfate concentration and injections data:
+
+# Clean sulfate_injections_data, format class:
+sulfate_injections_data <- clean_names(sulfate_injections_data)
+sulfate_injections_data$year <- ymd(sulfate_injections_data$year, truncated = 2L) #convert to date
+sulfate_injections_data$MW <- as.factor(sulfate_injections_data$well_id_injection_type)
+
+# Extract and format injection and push-pull dates from sulfate_injections_data for use in ggplot:
+injdates <- sulfate_injections_data %>% filter(type %in% c("Sulfate injection"))
+injdates$type <- as.factor(injdates$type)
+injdatesonly <- as.data.frame(injdates$year)
+injdatesonly <- as.data.frame(injdates$year) %>% mutate(inj_date = injdates$year)
+pushpulldates <- sulfate_injections_data %>% filter(type %in% c("Push-pull"))
+pushpulldates$type <- as.factor(pushpulldates$type)
+pushpulldatesonly <- as.data.frame(pushpulldates$year)
+pushpulldatesonly <- as.data.frame(pushpulldates$year) %>% mutate(push_date = pushpulldates$year)
+
+# Format sulfate_data column class:
 sulfate_data$Date <- as.Date(as.numeric(as.character(sulfate_data$Date)), origin = "1899-12-30")
 sulfate_data$Sulfate <- as.numeric(sulfate_data$Sulfate)
 sulfate_data$MW <- as.factor(sulfate_data$`Monitoring Well`)
 
+# Filter sulfate_data to remove duplicate samples, keeping only the highest result; and 
+# remove data from Monitoring Well "N" which is not considered to have independent concentration results. 
 sulfate_data2 <- sulfate_data %>% group_by(MW, Date) %>% filter(Sulfate == max(Sulfate)) %>% 
   filter(!MW %in% "N")
 
-#Calculate mean sitewide sulfate concentrations by year:
+# Calculate mean sitewide sulfate concentrations by year:
 sulfatemeans <- sulfate_data2 %>% group_by(year = floor_date(Date, "year")) %>%
   summarize(meansulfate = mean(Sulfate))
 
-#Calculate yearly mean sulfate concentrations by well:
+# Calculate yearly mean sulfate concentrations by well:
 sulfatemeansbywell <- sulfate_data2 %>% group_by(MW, year = floor_date(Date, "year")) %>%
   summarize(meansulfate = mean(Sulfate))
 
-#before 2012: 
+# Calculate sitewide baseline (pre-2012) sulfate concentrations:
 meansulfatebaseline <- sulfate_data2 %>% filter(Date < '2012-01-01')
 meansulfatebaseline <- mean(meansulfatebaseline$Sulfate)
-# 27.85
 
 
-# Sulfate by Injection Well:
+
+# Plot sulfate concentrations by Injection Well (PLOT 3):
 ggplot(sulfate_data2 %>% filter(MW %in% c("A", "B", "C", "D", "E", "F", "G", "H", "L")), 
        aes(Date, Sulfate, color = MW)) +
   geom_line(na.rm = TRUE) +
@@ -151,8 +156,7 @@ ggplot(sulfate_data2 %>% filter(MW %in% c("A", "B", "C", "D", "E", "F", "G", "H"
   ylab("Sulfate (mg/L, log scale)") +
   scale_x_date(date_breaks = "4 years", date_labels = "%Y") 
 
-# plot sulfate concentrations by pad-edge well: 
-
+# Plot sulfate concentrations by pad-edge well (PLOT 4): 
 ggplot(sulfate_data2 %>% filter(MW %in% c("I", "J", "K", "M")), 
        aes(Date, Sulfate, color = MW)) +
   geom_line(na.rm = TRUE) +
